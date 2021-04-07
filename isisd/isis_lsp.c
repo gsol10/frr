6750a133a18a58accd6f3f6966192c26e990637f
Guillaume Solignac <guillaume.solignac@orange.com>
2021-04-07 12:06:28 +0200

    Fix early PSNP for unfilled early

--- isisd/isis_lsp.c
+++ isisd/isis_lsp.c
@@ -2117,6 +2117,7 @@ void lsp_set_ssnflag(uint32_t SSNflags[ISIS_MAX_CIRCUITS],
 {
 	if (!ISIS_CHECK_FLAG(SSNflags, circuit)) {
 		ISIS_SET_FLAG(SSNflags, circuit);
+		thread_cancel(&circuit->t_send_psnp[level - 1]);
 		if (circuit->fp_lsp_with_ssnflag[level - 1]++ == 0) {
 			if (level == ISIS_LEVEL1) {
 				thread_add_timer_msec(
